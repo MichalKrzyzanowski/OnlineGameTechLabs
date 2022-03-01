340fbd960cc5ea7fe7399122e7e93b21dc3ff7e0
MichalKrzyzanowski <c00240696@itcarlow.ie>
2022-03-01 18:49:29 +0000

    finished lab 9

--- Project1/Tut13_Client/Source.cpp
+++ Project1/Tut13_Client/Source.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 	Client myClient("127.0.0.1",1111); //Create client to connect to server 127.0.0.1 [localhost] on port 1111
 	if (!myClient.Connect()) //If client fails to connect...
 	{
-		std::cout << "Failed to connect to server..." << std::endl;
+		std::cout << "Failed to connect to server..." << std::endl
 		system("pause");
 		return -1;
 	}
